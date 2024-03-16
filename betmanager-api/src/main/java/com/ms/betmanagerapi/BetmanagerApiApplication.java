39fcbc2cca5ae697678cffbca33ecfb20eaadb0f
eduardoserpa1 <dudu.rserpa@gmail.com>
2024-03-16 03:28:17 -0300

    add spring base code for api and postgres database settings

---
+++ betmanager-api/src/main/java/com/ms/betmanagerapi/BetmanagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ms.betmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetmanagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetmanagerApiApplication.class, args);
+    }
+
+}
